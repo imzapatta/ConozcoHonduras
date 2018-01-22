cec5e825528ca344e5637bd4a66b619d79bcaa9c
Rob Zapata <imzapatta@gmail.com>
2018-01-22 04:04:12 -0600

    Juego Funcional
    Primera Versión Estable del juego.

---
+++ activity.py
@@ -0,0 +1,9 @@
+from olpcgames import activity
+from gettext import gettext as _
+
+class Activity(activity.PyGameActivity):
+    """Your Sugar activity"""
+    
+    game_name = 'conozcohn'
+    game_title = _('Conozco Honduras')
+    game_size = (1200,900)
